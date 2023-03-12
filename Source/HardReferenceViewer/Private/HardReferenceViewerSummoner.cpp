00155e3f59920c51c54e4ae209106919c72f393b
Omid Kiarostami <okiarostami@hotmail.com>
2023-03-12 12:35:19 -0700

    Stub in a way for searching a graph for package references

--- Source/HardReferenceViewer/Private/HardReferenceViewerSummoner.cpp
+++ Source/HardReferenceViewer/Private/HardReferenceViewerSummoner.cpp
@@ -3,6 +3,7 @@
 #include "HardReferenceViewerSummoner.h"
 #include "HardReferenceViewerStyle.h"
 #include "BlueprintEditor.h"
+#include "K2Node_CallFunction.h"
 #include "SSubobjectEditor.h"
 #include "AssetRegistry/AssetRegistryModule.h"
 
@@ -24,41 +25,68 @@ FHardReferenceViewerSummoner::FHardReferenceViewerSummoner(TSharedPtr<FAssetEdit
 
 TSharedRef<SWidget> FHardReferenceViewerSummoner::CreateTabBody(const FWorkflowTabSpawnInfo& Info) const
 {
+	// stub a path to asset data / dependencies from the blueprint editor 
+	TSharedPtr<FBlueprintEditor> BlueprintEditorPtr = StaticCastSharedPtr<FBlueprintEditor>(HostingApp.Pin());
+	TSharedPtr<SSubobjectEditor> SubobjectEditorPtr = BlueprintEditorPtr->GetSubobjectEditor();
+	SSubobjectEditor* SubobjectEditorWidget = SubobjectEditorPtr.Get();
+
+	UObject* Object = SubobjectEditorWidget->GetObjectContext();
+	FString ObjectPath = Object->GetPathName();
+
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::Get().LoadModuleChecked<FAssetRegistryModule>(TEXT("AssetRegistry"));
+	FAssetData ExistingAsset = AssetRegistryModule.Get().GetAssetByObjectPath(FSoftObjectPath(ObjectPath));
+
+	TArray<FName> Dependencies;
+	UE::AssetRegistry::FDependencyQuery Flags(UE::AssetRegistry::EDependencyQuery::Hard);
+	AssetRegistryModule.GetDependencies(ExistingAsset.PackageName, Dependencies, UE::AssetRegistry::EDependencyCategory::Package, Flags);
+
+	// stub a calculation of size for dependencies
+	int64 TotalSize = 0;
+	for(const FName& Dependency : Dependencies)
 	{
-		// stub code to demonstrates a path to asset data from the blueprint editor 
-		TSharedPtr<FBlueprintEditor> BlueprintEditorPtr = StaticCastSharedPtr<FBlueprintEditor>(HostingApp.Pin());
-		TSharedPtr<SSubobjectEditor> SubobjectEditorPtr = BlueprintEditorPtr->GetSubobjectEditor();
-		SSubobjectEditor* SubobjectEditorWidget = SubobjectEditorPtr.Get();
+		FAssetPackageData AssetPackageData;
+		AssetRegistryModule.TryGetAssetPackageData(Dependency, AssetPackageData);
 
-		UObject* Object = SubobjectEditorWidget->GetObjectContext();
-		FString ObjectPath = Object->GetPathName();
+		TotalSize += AssetPackageData.DiskSize;
+	}
 
-		FAssetRegistryModule& AssetRegistryModule = FModuleManager::Get().LoadModuleChecked<FAssetRegistryModule>(TEXT("AssetRegistry"));
-		FAssetData ExistingAsset = AssetRegistryModule.Get().GetAssetByObjectPath(FSoftObjectPath(ObjectPath));
-
-		TArray<FName> Dependencies;
-		UE::AssetRegistry::FDependencyQuery Flags(UE::AssetRegistry::EDependencyQuery::Hard);
-		AssetRegistryModule.GetDependencies(ExistingAsset.PackageName, Dependencies, UE::AssetRegistry::EDependencyCategory::Package, Flags);
-
-		// stub in a calculation of size for dependencies
-		int64 TotalSize = 0;
-		for(const FName& Dependency : Dependencies)
+	// stub in a way to search blueprints for references to dependencies
+	TArray<UEdGraphNode*> ReferencingNodes;
+	if( UBlueprint* Blueprint = BlueprintEditorPtr->GetBlueprintObj() )
+	{
+		for(UEdGraph* Graph : Blueprint->UbergraphPages)
 		{
-			FAssetPackageData AssetPackageData;
-			AssetRegistryModule.TryGetAssetPackageData(Dependency, AssetPackageData);
-
-			TotalSize += AssetPackageData.DiskSize;
+			if(Graph)
+			{
+				for (UEdGraphNode* Node : Graph->Nodes)
+				{
+					if(UK2Node_CallFunction* CallFunctionNode = Cast<UK2Node_CallFunction>(Node))
+					{
+						if( UPackage* FunctionPackage = CallFunctionNode->FunctionReference.GetMemberParentPackage() )
+						{
+							FPackageId PackageId = FunctionPackage->GetPackageId();
+							FString PackagePathName = FunctionPackage->GetPathName(); 
+							FAssetData PackageAssetData = AssetRegistryModule.Get().GetAssetByObjectPath(PackagePathName);
+							if(Dependencies.ContainsByPredicate([PackageAssetData](const FName& ThisName)->bool	{ return ThisName.ToString() == PackageAssetData.PackageName.ToString(); }))
+							{
+								// Really crude, but works for now.  There is a better way to get to this.
+								ReferencingNodes.Add(Node);
+							}
+						}
+					}
+					// @omidk TODO: Also check Cast nodes
+				}
+			}
 		}
-		
-		// TODO:
-		//		- Find a nice way to format the window
-		//		- Find a way to search the graph for package references
-		
-		FText StubText = FText::Format(LOCTEXT("EmptyTabMessage", "This object has {0} references to other packages. TotalSize={1}"), Dependencies.Num(), TotalSize);
-		return SNew(STextBlock).Text(StubText);
 	}
 	
-	return FWorkflowTabFactory::CreateTabBody(Info);
+
+	// TODO:
+	//		- Find a nice way to format the window
+	//		- Find a NICE way to search the graph for package references
+
+	FText StubText = FText::Format(LOCTEXT("EmptyTabMessage", "This object has {0} references to other packages. TotalSize={1}"), Dependencies.Num(), TotalSize);
+	return SNew(STextBlock).Text(StubText);
 }
 
 FText FHardReferenceViewerSummoner::GetTabToolTipText(const FWorkflowTabSpawnInfo& Info) const
